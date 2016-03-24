19a4f98d4a2a9012cfb3576bd90f58011e8f0090
Bak <shaia.bak@hotmail.com>
2016-03-24 05:10:00 -0700

    Initial Git Build

---
+++ DanceRumble-Prototype01/Assets/Scripts/GameController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum generatedDir {
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+public class GameController : MonoBehaviour {
+    public Camera cam;
+    public Text AIDirText;
+    public Text timerText;
+
+    public int dirNumber;
+    public float waitBeforeStart;
+    public float waitBetweenDir;
+    public float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+	    if(cam == null) {
+            cam = Camera.main;
+        }
+        UpdateTimerText();
+        StartCoroutine(spawnDirection());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) {
+            timeLeft = 0;
+        }
+        UpdateTimerText();
+	}
+
+    IEnumerator spawnDirection() {
+        yield return new WaitForSeconds(waitBeforeStart);
+        
+        while(timeLeft > 0) {
+            dirNumber = Random.Range(1, 5);
+            UpdateDirText();
+            yield return new WaitForSeconds(waitBetweenDir);
+        }
+        //yield return new WaitForSeconds(3.0f);
+    }
+
+    void UpdateTimerText() {
+        timerText.text = "Time: " + Mathf.RoundToInt(timeLeft);
+    }
+
+    void UpdateDirText() {
+        AIDirText.text = "Game Direction\n" + dirNumber;
+    }
+}
---
+++ DanceRumble-Prototype01/Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum SwipeDirection {
+    None = 0,
+    Left = 1,
+    Right = 2,
+    Up = 3,
+    Down = 4
+}
+
+public class SwipeDetection : MonoBehaviour {
+
+    private static SwipeDetection instance;
+    public static SwipeDetection Instance { get { return instance; } }
+
+    // Direction is a PROPERTY
+    public SwipeDirection Direction { set; get; }
+
+    private Vector3 touchPosition;
+    public float swipeResistanceX;
+    public float swipeResistanceY;
+    public Text dirText;
+
+    private void Start() {
+        instance = this;
+    }
+
+    private void Update() {
+        Direction = SwipeDirection.None;
+
+        // when mouse left clicks
+        if (Input.GetMouseButtonDown(0)) {
+            // the touch position is moved to where the mouse position is
+            touchPosition = Input.mousePosition;
+        }
+
+        // when mouse is released
+        if (Input.GetMouseButtonUp(0)) {
+            // deltaSwipe is the difference to where you touched and you released
+            Vector2 deltaSwipe = touchPosition - Input.mousePosition;
+
+            // check how far / direction
+            if (Mathf.Abs(deltaSwipe.x) > swipeResistanceX) {
+                // Swipe on the X axis
+                Direction |= (deltaSwipe.x < 0) ? SwipeDirection.Right : SwipeDirection.Left; // if x is smaller than 0, swipdirection is right, else Left
+                if (Direction == SwipeDirection.Left) {
+                    dirText.text = "Player Direction:\nLeft";
+                } else if (Direction == SwipeDirection.Right) {
+                    dirText.text = "Player Direction:\nRight";
+                }
+            }
+
+            if (Mathf.Abs(deltaSwipe.y) > swipeResistanceY) {
+                // Swipe on the Y axis
+                Direction |= (deltaSwipe.y < 0) ? SwipeDirection.Up : SwipeDirection.Down;
+                if (Direction == SwipeDirection.Up) {
+                    dirText.text = "Player Direction:\nUp";
+                } else if (Direction == SwipeDirection.Down) {
+                    dirText.text = "Player Direction:\nDown";
+                }
+            }
+        }
+    }
+
+    public bool IsSwiping(SwipeDirection dir) {
+        return (Direction & dir) == dir;
+    }
+}
