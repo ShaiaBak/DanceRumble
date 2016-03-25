7b69032bc7c854d2f18766e1f70f54b5e92dc7d4
Bak <shaia.bak@hotmail.com>
2016-03-25 05:41:59 -0700

    refactored generated direction to own script

--- DanceRumble-Prototype01/Assets/Scripts/GameController.cs
+++ DanceRumble-Prototype01/Assets/Scripts/GameController.cs
@@ -2,29 +2,12 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public enum generatedDir {
-	None,
-	Up,
-	Right,
-    Down,
-	Left
-}
-
 public class GameController : MonoBehaviour {
     public Camera cam;
-    public Text AIDirText;
     public Text timerText;
 	public Text debugText;
-	public Text switchNumText;
-	public int switchNum = 0;
 
-    public int dirNumber;
-    public float waitBeforeStart;
-    public float waitBetweenDir;
     public float timeLeft;
-
-	public string genPrevDir; // generated previous direction 
-	public generatedDir genCurrDirection { set; get; } // generated current direction
 
 	// Use this for initialization
 	void Start () {
@@ -33,7 +16,6 @@ public class GameController : MonoBehaviour {
         }
         UpdateTimerText();
 		InitDebugText();
-        StartCoroutine(spawnDirection());
     }
 	
 	// Update is called once per frame
@@ -46,44 +28,8 @@ public class GameController : MonoBehaviour {
         UpdateTimerText();
 	}
 
-    IEnumerator spawnDirection() {
-        yield return new WaitForSeconds(waitBeforeStart);
-        
-        while(timeLeft > 0) {
-            dirNumber = Random.Range(1, 5);
-			gameDirectionHandler ();
-            UpdateDirText();
-            yield return new WaitForSeconds(waitBetweenDir);
-        }
-        //yield return new WaitForSeconds(3.0f);
-    }
-
-	void gameDirectionHandler() {
-		genCurrDirection =  generatedDir.None; 
-		switch (dirNumber) {
-		case 1:
-			genCurrDirection = generatedDir.Up;
-			break;
-		case 2:
-			genCurrDirection = generatedDir.Right;
-			break;
-		case 3:
-			genCurrDirection = generatedDir.Down;
-			break;
-		case 4:
-			genCurrDirection = generatedDir.Left;
-			break;
-		}
-	}
-
     void UpdateTimerText() {
         timerText.text = "Time: " + Mathf.RoundToInt(timeLeft);
-    }
-
-    void UpdateDirText() {
-		switchNum++;
-		AIDirText.text = "Game Direction\n" + genCurrDirection;
-		switchNumText.text = "Switch: " + switchNum;
     }
 
 	void InitDebugText() {
---
+++ DanceRumble-Prototype01/Assets/Scripts/GenerateDirection.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum generatedDir {
+    None,
+    Up,
+    Right,
+    Down,
+    Left
+}
+
+public class GenerateDirection : MonoBehaviour {
+
+    public GameController gameController;
+
+    private static GenerateDirection instance;
+    public static GenerateDirection Instance { get { return instance; } }
+
+    public int dirNumber;
+    public float waitBeforeStart;
+    public float waitBetweenDir;
+
+    public Text switchNumText;
+    public Text AIDirText;
+    public int switchNum = 0;
+
+    public string genPrevDir; // generated previous direction 
+    public generatedDir genCurrDirection { set; get; } // generated current direction
+
+    // Use this for initialization
+    void Start () {
+        instance = this;
+        gameController = GetComponent<GameController>();
+        StartCoroutine(spawnDirection());
+    }
+
+    IEnumerator spawnDirection() {
+        yield return new WaitForSeconds(waitBeforeStart);
+
+        while (gameController.timeLeft > 0) {
+            dirNumber = Random.Range(1, 5);
+            gameDirectionHandler();
+            UpdateDirText();
+            yield return new WaitForSeconds(waitBetweenDir);
+        }
+        //yield return new WaitForSeconds(3.0f);
+    }
+
+    void gameDirectionHandler() {
+        genCurrDirection = generatedDir.None;
+        switch (dirNumber) {
+            case 1:
+                genCurrDirection = generatedDir.Up;
+                break;
+            case 2:
+                genCurrDirection = generatedDir.Right;
+                break;
+            case 3:
+                genCurrDirection = generatedDir.Down;
+                break;
+            case 4:
+                genCurrDirection = generatedDir.Left;
+                break;
+        }
+    }
+
+    public bool GenSwipe(generatedDir dir) {
+        return (genCurrDirection & dir) == dir;
+    }
+
+    void UpdateDirText() {
+        switchNum++;
+        AIDirText.text = "Game Direction\n" + genCurrDirection;
+        switchNumText.text = "Switch: " + switchNum;
+    }
+}
